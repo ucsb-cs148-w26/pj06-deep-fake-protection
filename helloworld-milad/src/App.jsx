8f723134fb8778dc782f6bb06e2fcd1d8bfe04b5
Milad Haghighi <mhaghighi@ucsb.edu>
2026-01-14 14:50:13 -0800

    milad hello world

--- helloworld-milad/src/App.jsx
+++ helloworld-milad/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello World</h1>
+      <h1>Hello World.</h1>
     </div>
   )
 }
